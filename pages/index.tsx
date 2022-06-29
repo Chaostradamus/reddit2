24e3e72cb75c40d62a8b2c5f2ba17f25213d316c
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2022-06-29 02:12:53 -0400

    30606

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Feed from "../components/Feed";
 import PostBox from "../components/PostBox";
 
 const Home: NextPage = () => {
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
       <PostBox />
 
       <div className='flex'>{/* feed */}</div>
+      <Feed />
     </div>
   );
 };
